a79a70d4a8182026262ce34fca5d1ea6c3b60cb3
Rishi Dholkheria <57285705+rishidholkheria@users.noreply.github.com>
2021-04-25 14:58:21 +0530

    Doughnut Chart UI

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Searchbox from "../../components/Searchbox/Searchbox";
 import "./Dashboard.css";
+import { Doughnut } from "react-chartjs-2";
 
 const Dashboard = () => {
+  const [chartData, setChartData] = useState({});
+  const chart = () => {
+    setChartData({
+      labels: ["Added", "Dispatched", "Returned"],
+      datasets: [
+        {
+          data: [32, 43, 12],
+          backgroundColor: [
+            "rgb(10%, 68%, 47%)",
+            "rgb(57%, 35%, 81%)",
+            "rgb(91%, 16%, 16%)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    });
+  };
+
+  useEffect(() => {
+    chart();
+  }, []);
+
   return (
     <div className="dashboard">
       <div className="dashboard__head">
@@ -53,6 +76,19 @@ const Dashboard = () => {
             <p className="number_of_item">200</p>
             <p className="detail_heading">Items Returned</p>
           </div>
+        </div>
+      </div>
+      <div className="stock_analytics">
+        <div className="stock_chart">
+          <Doughnut
+            data={chartData}
+            options={{
+              responsive: true,
+            }}
+          />
+        </div>
+        <div className="stock_numbers">
+          <p>Stock Analytics</p>
         </div>
       </div>
     </div>
